462ddd28830b9ae2445805e8bc63185517481337
ahmed-shiiqaaye <me.shiqaye@gmail.com>
2022-06-17 01:40:31 +0200

    about page

--- js/app.js
+++ js/app.js
@@ -3,15 +3,30 @@ let nextbtn = document.querySelectorAll('.nextBtn');
 let prevBtn = document.querySelectorAll('.prevBtn');
 let wrapperCard =document.querySelector('.wrappers')
 
-let wrapperCardw = wrapperCard.getBoundingClientRect().width;
-console.log(wrapperCardw);
-
 SliderContainer.forEach((conttainer,i,wrapperCard)=>{
     let conttainerWith = conttainer.getBoundingClientRect().width;
     nextbtn[i].addEventListener('click',()=>{
-        conttainer.scrollLeft += wrapperCardw
+        conttainer.scrollLeft += 400
     })
     prevBtn[i].addEventListener('click',()=>{
-        conttainer.scrollLeft -= wrapperCardw
+        conttainer.scrollLeft -= 400
+    })
+})
+
+// TabContents
+let tabContents = document.querySelectorAll('.tab-content');
+let tabButtons = document.querySelectorAll('.tab-btn');
+
+tabButtons.forEach((btn,i)=>{
+    btn.addEventListener('click',()=>{
+        tabButtons.forEach((btnn,i)=>{
+               btnn.classList.remove('active')   
+        })
+        tabContents.forEach((content,i)=>{
+               content.classList.remove('active')   
+        })
+        
+        tabButtons[i].classList.add('active')
+        tabContents[i].classList.add('active')
     })
 })
